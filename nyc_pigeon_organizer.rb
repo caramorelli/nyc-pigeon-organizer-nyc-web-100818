ec39874c7425919d471cb9b0bb903ab180fc93d6
Cara Morelli <caramorelli7@gmail.com>
2018-09-29 23:10:36 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   pigeon_by_name = {}
   name_arr = data[:gender][:male] += data[:gender][:female] #["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky", "Queenie", "Ms. K"]
   name_arr.each do |name|
-    pigeon_by_name[name] = data.keys.map { |ele| ele = {}}
+    pigeon_by_name[name] = {} 
     # puts data.keys
   
   end 
