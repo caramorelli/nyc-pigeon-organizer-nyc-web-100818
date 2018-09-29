6e12af59c16067a9543b656e3c17b36a36c3eaab
Cara Morelli <caramorelli7@gmail.com>
2018-09-29 23:29:50 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,8 +6,9 @@ def nyc_pigeon_organizer(data)
   end 
   data.each do |key, val|
     val.each do |k, v|
-      print v
-      puts ''
+      v.each do |names|
+        pigeon_by_name[names]
+
       # pigeon_by_name[v] << k
     end 
   end 
